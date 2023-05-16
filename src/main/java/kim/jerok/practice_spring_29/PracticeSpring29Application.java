6afdae188616f7ebd6b69621be4b00395ed26399
jerok <jerok.kim@gmail.com>
2023-05-17 00:04:39 +0900

    practice spring

---
+++ src/main/java/kim/jerok/practice_spring_29/PracticeSpring29Application.java
@@ -0,0 +1,13 @@
+package kim.jerok.practice_spring_29;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpring29Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeSpring29Application.class, args);
+    }
+
+}
